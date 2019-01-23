7876bdc5f6f50bd7022885ab05b8232515e814f3
Arthur Lam <arthurlam@bc.holdings>
2019-01-23 10:53:08 +0800

    https://www.careercup.com/question?id=5638939143045120

--- coinStack.cs
+++ coinStack.cs
@@ -3,19 +3,34 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
-// To execute C#, please define "static void Main" on a class
-// named Solution.
+// Source
+// https://www.careercup.com/question?id=5638939143045120
+
+// Question
+// #################################
+// Given many coins of 3 different face values, print the combination sums of the coins up to 1000. Must be printed in order. 
+//
+// eg: coins(10, 15, 55) 
+// print: 
+// 10 
+// 15 
+// 20 
+// 25 
+// 30 
+// . 
+// . 
+// . 
+// 1000
+
 
 class Solution
 {
-    
     static void Main(string[] args)
     {
         int[] coinTypes = {10, 15, 55};   
         bool[] coinMap = new bool[1000];
         
-        for (int i = 0; i<1000; i++) {
-        
+        for (int i = 0; i<1000; i++) {       
             if(coinTypes.ToList().Any(x => (i==x) || ((i >= x) && coinMap[i-x] == true))) {
                 
                 coinMap[i] = true;
